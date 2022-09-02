f851ecc221a11e2fd701cdd391f816a602e2b8b3
Daniel Tejeda <daviddan1998@gmail.com>
2022-09-01 20:40:44 -0400

    Added route files, created errors json file, created first route.

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ After pulling the code from the repository
 1. Run ```npm init```
 2. Set up a .env file. Read about creating it [here](https://www.codementor.io/@parthibakumarmurugesan/what-is-env-how-to-set-up-and-run-a-env-file-in-node-1pnyxw9yxj) 
 3. Set up ```PORT = desiredport #```. NOTE: IT IS IMPORTANT TO USE **PORT** AS THE VARIABLE NAME
+
+## Route files
+
+admin.js - handles all /admin/ routes
+api.js - handles all /api/ routes
--- index.js
+++ index.js
@@ -3,15 +3,26 @@ const cors = require("cors")
 const _ = require("lodash")
 const {v4: uuid} = require("uuid")
 require('dotenv').config()
+const errors = require('./util/errors.json')
 
 const app = express()
 
+app.use('/', (req, res, next) =>
+{
+  res
+    .status(200)
+    .json(
+      {
+        message: "We're live!"
+      })
+})
+//Check if we have the PORT env variable set up
 try {
-  if(process.env.PORT)
+  if(!process.env.PORT)
   {
-    app.listen(process.env.PORT, () => console.log("API Server is running..." + process.env.PORT))
+    throw errors.NoPortOnEnvDetected
   }
-  throw "Server start error: No PORT environment variable has been set up!"
+  app.listen(process.env.PORT, () => console.log("API Server is running..." + process.env.PORT))
 }
 catch (e) {
   console.log(e)
