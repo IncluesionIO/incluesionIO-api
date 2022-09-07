727770b1ef623e395594b5363b2223c38a7f1b2f
Daniel Tejeda <daviddan1998@gmail.com>
2022-09-06 20:48:17 -0400

    created user schema test cases

--- tests/user.spec.js
+++ tests/user.spec.js
@@ -6,5 +6,80 @@ const ValidationError = mongoose.Error.ValidationError
 const User = require('../models/user.model')
 
 describe('Testing User model', () => {
+  let sampleUser, sampleUserAdmin
+
+  beforeEach(() =>
+  {
+    sampleUser = {
+      username: 'testUser1',
+      password: 'testPassword1',
+      name: 'Test User1'
+    }
+
+    sampleUserAdmin = {
+      username: 'testUserAdmin1',
+      password: 'testPassword12',
+      name: 'Test UserAdmin1',
+      role: 'ADMIN'
+    }
+  })
+
+  it('it should throw an error due to missing fields', (done) =>
+  {
+    let user = new User()
+    
+    user.validate((err) =>
+    {
+      expect(err.errors.username).to.exist
+      expect(err.errors.password).to.exist
+      expect(err.errors.name).to.exist
+      //Role should be default
+      expect(err.errors.role).to.not.exist
   
+      done()
+    })
+  })
+
+
+  it('it should successfully create the user', done =>
+  {
+    let user = new User(sampleUser)
+
+    user.validate((err) =>
+    {
+      if(err)
+      {
+        const unexpectedFailureError = new Error('🔥🔥🔥Unexpected Failure🔥🔥🔥')
+        done(unexpectedFailureError)
+      }
+      else {
+        expect(user.username).to.equal(sampleUser.username)
+        expect(user.password).to.equal(sampleUser.password)
+        expect(user.name).to.equal(sampleUser.name)
+        expect(user.role).to.equal('USER')
+        done()
+      }
+    })
+  })
+
+  it('it should successfully create an admin user', done =>
+  {
+    let user = new User(sampleUserAdmin)
+
+    user.validate((err) =>
+    {
+      if(err)
+      {
+        const unexpectedFailureError = new Error('🔥🔥🔥Unexpected Failure🔥🔥🔥')
+        done(unexpectedFailureError)
+      }
+      else {
+        expect(user.username).to.equal(sampleUserAdmin.username)
+        expect(user.password).to.equal(sampleUserAdmin.password)
+        expect(user.name).to.equal(sampleUserAdmin.name)
+        expect(user.role).to.equal(sampleUserAdmin.role)
+        done()
+      }
+    })
+  })
 })
