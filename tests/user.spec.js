b470b4e444e9660283c67c37d4e5dedb4307d567
Daniel Tejeda <daviddan1998@gmail.com>
2022-09-06 20:24:43 -0400

    Added Mocha and Chai, created user schema

---
+++ tests/user.spec.js
@@ -0,0 +1,10 @@
+const chai = require('chai')
+const expect = chai.expect
+const mongoose = require('mongoose')
+const ValidationError = mongoose.Error.ValidationError
+
+const User = require('../models/user.model')
+
+describe('Testing User model', () => {
+  
+})
