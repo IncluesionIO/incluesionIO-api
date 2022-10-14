8e9cabc06dddb8fad878a1480c95108074e698c6
Daniel Tejeda <daviddan1998@gmail.com>
2022-10-14 14:58:18 -0400

    assessment swagger documentation

--- routes/assessment.route.js
+++ routes/assessment.route.js
@@ -3,7 +3,37 @@ const { body } = require("express-validator");
 const router = express.Router();
 
 const assessmentController = require('../controllers/assessment.controller')
-
+/**
+ * @swagger
+ * /assessment/submitAssessment:
+ *   post:
+ *     tags: 
+ *       - Assessment
+ *     summary: Submit assessment answers
+ *     description: Endpoint to record assessment answers
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: assessment
+ *         schema:  
+ *           type: object
+ *           required:
+ *             - timestamp
+ *             - data
+ *           properties:
+ *              timestamp:
+ *                type: string
+ *              data:
+ *                type: object
+ *     responses:
+ *       '200':
+ *          description: A successful request, assessment recorded
+ *       '422':
+ *          description: There was an error in the parameters
+ *       '500': 
+ *          description: Internal server error
+ */
 router.post('/submitAssessment', 
 [
   body('timestamp')
