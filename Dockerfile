9b93375a2bea8c2005717bb60977a3aee8fff67f
Daniel Tejeda <daviddan1998@gmail.com>
2022-11-08 20:54:12 -0500

    dockerize mongodb, fixed issues with mounting app as a volume

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 FROM node:16
 
+RUN mkdir -p /usr/src/app
+
 WORKDIR /usr/src/app
 
-COPY package*.json ./
+COPY . .
 
 RUN npm install
-
-COPY . .
 
 EXPOSE 3000
 
--- index.js
+++ index.js
@@ -1,83 +1,81 @@
-const express = require("express")
-const cors = require("cors")
-const _ = require("lodash")
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const {v4: uuid} = require("uuid")
-require('dotenv').config()
-
+const express = require("express");
+const cors = require("cors");
+const _ = require("lodash");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { v4: uuid } = require("uuid");
+require("dotenv").config();
+//Import database config
+const DBURI = require("./config/db.config").url;
 
 //Routes
-const apiDocRoute = require('./routes/api-doc.route')
-const adminRoutes = require('./routes/admin.route')
-const authRoutes = require('./routes/auth.route')
-const userRoutes = require('./routes/user.route')
-const assessmentRoutes = require('./routes/assessment.route')
-const errors = require('./util/errors.json')
-const {errorResponseHandler} = require('./util/errorHandler') 
+const apiDocRoute = require("./routes/api-doc.route");
+const adminRoutes = require("./routes/admin.route");
+const authRoutes = require("./routes/auth.route");
+const userRoutes = require("./routes/user.route");
+const assessmentRoutes = require("./routes/assessment.route");
+const errors = require("./util/errors.json");
+const { errorResponseHandler } = require("./util/errorHandler");
 
-const app = express()
+const app = express();
 
 //Example of auth required route
-const isAuth = require('./middleware/isAuth')
+const isAuth = require("./middleware/isAuth");
 
-app.use('/authenticated', isAuth, (req, res, next) =>
-{
-  res.status(200).json({msg: "You're authenticated!"})
-})
+app.use("/authenticated", isAuth, (req, res, next) => {
+  res.status(200).json({ msg: "You're authenticated!" });
+});
 
 //Example of an error
-app.use('/error', (req, res, next) =>
-{
-  const err = new Error('Error')
-  err.httpStatus = 410
-  next(err)
-})
+app.use("/error", (req, res, next) => {
+  const err = new Error("Error");
+  err.httpStatus = 410;
+  next(err);
+});
 
 //Used to test server
 
-app.use(cors())
+app.use(cors());
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use('/user', userRoutes)
+app.use("/user", userRoutes);
 
-app.use('/api-docs', apiDocRoute)
+app.use("/api-docs", apiDocRoute);
 
-app.use('/auth', authRoutes)
+app.use("/auth", authRoutes);
 
-app.use('/admin', adminRoutes)
+app.use("/admin", adminRoutes);
 
-app.use('/assessment', assessmentRoutes)
+app.use("/assessment", assessmentRoutes);
 
-app.use('/', (req, res, next) =>
-{
-  res
-    .status(200)
-    .json(
-      {
-        message: "We're live!"
-      })
-})
+app.use("/", (req, res, next) => {
+  res.status(200).json({
+    message: "We're live!",
+  });
+});
 
 //Error handler
-app.use(errorResponseHandler)
+app.use(errorResponseHandler);
 
 //Check if we have the PORT env variable set up
 try {
-  if(!process.env.PORT)
-  {
-    throw errors.NoPortOnEnvDetected
+  if (!process.env.PORT) {
+    throw errors.NoPortOnEnvDetected;
   }
-  if(!process.env.DBURI)
-  {
-    throw errors.NoDBURIOnEnvDetected
+  if (!DBURI) {
+    throw errors.NoDBURIOnEnvDetected;
   }
-  mongoose.connect(process.env.DBURI)
-    .then(result => {
-      app.listen(process.env.PORT, () => console.log("API Server is running..." + process.env.PORT))
+  mongoose
+    .connect(DBURI)
+    .then((result) => {
+      app.listen(process.env.PORT, () =>
+        console.log("API Server is running..." + process.env.PORT)
+      );
     })
-}
-catch (e) {
-  console.log(e)
+    .catch((err) => {
+      throw err;
+    });
+} catch (e) {
+  console.log(e);
 }
